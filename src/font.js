bdc4b094868146a2b3311a178c9a85b9d66ff361
Boshen <boshenc@gmail.com>
2018-03-20 10:17:41 +0800

    将 font.js 转换成 es5

--- src/font.js
+++ src/font.js
@@ -1,46 +1,47 @@
-const snakecase = require('lodash.snakecase');
-const camelcase = require('lodash.camelcase');
+var snakecase = require('lodash.snakecase');
+var camelcase = require('lodash.camelcase');
 
-const createFontFace = (fontFamily) => (blob) => {
-  const {head} = document;
+var createFontFace = function createFontFace(fontFamily) {
+  return function (blob) {
+    var _document = document,
+        head = _document.head;
 
-  let fontUrl = URL.createObjectURL(blob),
-    fontFace = `@font-face { font-family:'${fontFamily}'; src:url('${fontUrl}') format('woff'); }`,
-    cssId = snakecase(fontFamily),
-    styleList = document.getElementsByName(cssId),
-    style = document.createElement('style');
+    var fontUrl = URL.createObjectURL(blob),
+        fontFace = '@font-face { font-family:\'' + fontFamily + '\'; src:url(\'' + fontUrl + '\') format(\'woff\'); }',
+        cssId = snakecase(fontFamily),
+        styleList = document.getElementsByName(cssId),
+        style = document.createElement('style');
 
-  if (styleList.length > 0) {
-    for (let i = 0; i < styleList.length; i++) {
-      head.removeChild(styleList[i]);
+    if (styleList.length > 0) {
+      for (var i = 0; i < styleList.length; i++) {
+        head.removeChild(styleList[i]);
+      }
     }
-  }
 
-  style.type = 'text/css'
-  style.setAttribute("name", cssId);
+    style.type = 'text/css';
+    style.setAttribute("name", cssId);
 
-  if (style.styleSheet) {
-    style.styleSheet.cssText = fontFace;
-  } else if (document.getBoxObjectFor) {
-    style.innerHTML = fontFace;
-  } else {
-    style.appendChild(document.createTextNode(fontFace));
-  }
+    if (style.styleSheet) {
+      style.styleSheet.cssText = fontFace;
+    } else if (document.getBoxObjectFor) {
+      style.innerHTML = fontFace;
+    } else {
+      style.appendChild(document.createTextNode(fontFace));
+    }
 
-  head.appendChild(style);
-}
+    head.appendChild(style);
+  };
+};
 
-const getUrl = (uri, fontFamily) => {
-  return `${uri + camelcase(fontFamily).toLowerCase()}-regular.woff`;
-}
+var getUrl = function getUrl(uri, fontFamily) {
+  return uri + camelcase(fontFamily).toLowerCase() + '-regular.woff';
+};
 
-const createGoogleFontLink = (font) => {
-  const url = getUrl('//youziku.arkie.cn/webfonts/en/', font);
-  fetch(url, { headers: {responseType: 'blob'}})
-    .then((res) => {
-      return res.blob()
-    })
-    .then(createFontFace(font));
-}
+var createGoogleFontLink = function createGoogleFontLink(font) {
+  var url = getUrl('//youziku.arkie.cn/webfonts/en/', font);
+  fetch(url, { headers: { responseType: 'blob' } }).then(function (res) {
+    return res.blob();
+  }).then(createFontFace(font));
+};
 
 module.exports.createGoogleFontLink = createGoogleFontLink;
