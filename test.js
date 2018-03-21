454d76ebf8ed79a50a5a8939ba0ba361b1df3d93
Boshen <boshenc@gmail.com>
2018-03-21 12:09:09 +0800

    添加测试脚本

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 const json = require('./tree.json');
 const { renderSvg } = require('./src/main.bs');
-renderSvg(json)
+renderSvg(json, { fonts: false })
   .then(console.log.bind(console))
   .catch(console.error.bind(console));
