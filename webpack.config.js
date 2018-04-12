f4b1d6ae07d07a091ae9a0aec67899146cb5e791
Boshen <boshenc@gmail.com>
2018-04-12 15:38:43 +0800

    删除 rollup, 使用 webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './test.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ]
+}
