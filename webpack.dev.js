bfc22403769fd4013618d889a804848ed04bc308
Boshen <boshenc@gmail.com>
2018-04-15 10:22:14 +0800

    add typescript

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,46 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = {
   mode: 'development',
+
   entry: './index.js',
+
+  devtool: 'eval-source-map',
+
   output: {
     path: __dirname + '/dist',
     filename: 'bundle.js'
   },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  serve: {
+    content: [__dirname + '/dist'],
+    dev: {
+      stats: {
+        modules: false,
+        chunks: false,
+        assets: false,
+      },
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      },
+    ],
+  },
+
   plugins: [
+    new ForkTsCheckerWebpackPlugin(),
+
     new HtmlWebpackPlugin(),
   ]
 }
